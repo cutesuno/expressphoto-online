69ea56dc44f11ade970ce1e100e97caf6068f90b
Mykhailo Cherneha <nebula@MacBook-Air-Mykhailo.local>
2025-04-26 17:31:09 +0200

    add success and cancel pages

---
+++ pages/cancel.tsx
@@ -0,0 +1,7 @@
+export default function CancelPage() {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-black text-white p-6">
+        <h1 className="text-4xl font-bold">Оплата скасована ❌</h1>
+      </div>
+    );
+  }
---
+++ pages/success.tsx
@@ -0,0 +1,7 @@
+export default function SuccessPage() {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-black text-white p-6">
+        <h1 className="text-4xl font-bold">Дякуємо за оплату! 🎉</h1>
+      </div>
+    );
+  }
