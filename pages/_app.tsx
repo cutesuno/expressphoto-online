7befa4ff902faf34725c063b48a82cb6c4a3906c
Mykhailo Cherneha <nebula@MacBook-Air-Mykhailo.local>
2025-04-26 15:17:16 +0200

    add background image and styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return (
+    <div className="bg-main">
+      <Component {...pageProps} />
+    </div>
+  )
 }
-
-export default MyApp
