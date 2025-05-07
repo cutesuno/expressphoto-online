7947b9ddc17c93483fb4c6afb0067ac60e2d9964
Mykhailo Cherneha <nebula@MacBook-Air-Mykhailo.local>
2025-05-07 05:19:36 +0200

    додов теги

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,34 @@
+import Head from 'next/head';
 import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ExpressPhoto Online</title>
+        <meta name="description" content="Онлайн-друк фотографій, фото на документи, ксерокопії та інше." />
+        
+        {/* Open Graph */}
+        <meta property="og:title" content="ExpressPhoto Online" />
+        <meta property="og:description" content="Онлайн-друк фотографій, фото на документи, ксерокопії та інше." />
+        <meta property="og:image" content="URL_до_картинки" />
+        <meta property="og:url" content="https://www.expressphoto.online" />
+        <meta property="og:type" content="website" />
+        
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="ExpressPhoto Online" />
+        <meta name="twitter:description" content="Онлайн-друк фотографій, фото на документи, ксерокопії та інше." />
+        <meta name="twitter:image" content="URL_до_картинки" />
+        
+        {/* SEO */}
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://www.expressphoto.online" />
+      </Head>
+      {/* Ваш контент сторінки */}
+    </>
+  );
+}
 
 export default function Document() {
   return (
