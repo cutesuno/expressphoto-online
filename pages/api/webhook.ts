b03a3f131f55e80b61fa9681a066a4969fcfa189
Mykhailo Cherneha <nebula@MacBook-Air-Mykhailo.local>
2025-05-09 16:33:26 +0200

    🔥 feat: додано Stripe Checkout і Telegram webhook

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -12,7 +12,7 @@ export const config = {
 };
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
+apiVersion: '2025-03-31.basil',
 });
 
 const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
